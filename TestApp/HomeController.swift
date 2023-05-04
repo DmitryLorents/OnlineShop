653379b61888e7ca62f6079693d4883b6f9cde51
DmitryLorents <zmey.task@gmail.com>
2023-05-04 22:35:23 +0600

    - initial settings of view

--- TestApp/HomeController.swift
+++ TestApp/HomeController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import SnapKit
 
-class HomeController: UIViewController {
+final class HomeController: UIViewController {
+    
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupOutlets()
+        setupConstraints()
+    }
+    
+    private func setupOutlets() {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .yellow
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+    }
+    
+    private func setupConstraints() {
+        
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        
     }
 
 
 }
 
+extension HomeController: UICollectionViewDelegate {
+    
+}
+
+extension HomeController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        UICollectionViewCell()
+    }
+    
+    
+}
