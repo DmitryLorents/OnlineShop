92bf20624fc5c4b262774ee2d6a7071169a699e2
DmitryLorents <zmey.task@gmail.com>
2023-05-05 15:30:54 +0600

    - LatestCollectionViewCell created

---
+++ TestApp/Cell's/LatestCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  LatestCollectionViewCell.swift
+//  TestApp
+//
+//  Created by Дмитрий Лоренц on 05.05.2023.
+//
+
+import UIKit
+
+class LatestCollectionViewCell: UICollectionViewCell {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
