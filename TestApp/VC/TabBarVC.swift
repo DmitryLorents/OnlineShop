b38d83e03799c02b7fc31474427023c3ad2e581b
DmitryLorents <zmey.task@gmail.com>
2023-05-06 17:54:30 +0600

    - add remains VC to TabBar

--- TestApp/VC/TabBarVC.swift
+++ TestApp/VC/TabBarVC.swift
@@ -11,10 +11,32 @@ class TabBarVC: UITabBarController {
     
     fileprivate func setTabBar() {
 
+        view.backgroundColor = .white
         let homeVC = HomeController()
         let homeItem = UITabBarItem(title: nil, image: UIImage(named: "homeIcon"), selectedImage: UIImage(named: "homeIconActive")?.withRenderingMode(.alwaysOriginal))
         homeVC.tabBarItem = homeItem
-        setViewControllers([homeVC], animated: true)
+        
+        let hurtVC = UIViewController()
+        hurtVC.view.backgroundColor = .lightGray
+        let hurtItem = UITabBarItem(title: nil, image: UIImage(named: "hurtIcon"), selectedImage: UIImage(named: "hurtIcon")?.withRenderingMode(.alwaysOriginal))
+        hurtVC.tabBarItem = hurtItem
+        
+        let cartVC = UIViewController()
+        cartVC.view.backgroundColor = .lightText
+        let cartItem = UITabBarItem(title: nil, image: UIImage(named: "cartIcon"), selectedImage: UIImage(named: "cartIcon")?.withRenderingMode(.alwaysOriginal))
+        cartVC.tabBarItem = cartItem
+        
+        let noticetVC = UIViewController()
+        noticetVC.view.backgroundColor = .systemGray6
+        let noticeItem = UITabBarItem(title: nil, image: UIImage(named: "noticeIcon"), selectedImage: UIImage(named: "noticeIcon")?.withRenderingMode(.alwaysOriginal))
+        noticetVC.tabBarItem = noticeItem
+        
+        let personVC = UIViewController()
+        personVC.view.backgroundColor = .systemBackground
+        let personItem = UITabBarItem(title: nil, image: UIImage(named: "personIcon"), selectedImage: UIImage(named: "personIconActive")?.withRenderingMode(.alwaysOriginal))
+        personVC.tabBarItem = personItem
+        
+        setViewControllers([homeVC, hurtVC, cartVC,noticetVC,personVC], animated: true)
     }
     
     override func viewDidLoad() {
