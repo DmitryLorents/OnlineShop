51609bd7ded23dcb0b1c2be9d1be70b7355bfc64
DmitryLorents <zmey.task@gmail.com>
2023-06-27 21:50:20 +0300

    Save work2

--- TestApp/Cell's/LatestCollectionViewCell.swift
+++ TestApp/Cell's/LatestCollectionViewCell.swift
@@ -6,21 +6,39 @@
 //
 
 import UIKit
+import SnapKit
 
-class LatestCollectionViewCell: UICollectionViewCell {
+final class LatestCollectionViewCell: UICollectionViewCell {
     
     static let reuseID = "LatestCollectionViewCell"
+    var imageViewCell: UIImageView = {
+        let view = UIImageView()
+        return view
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setOutlets()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Methods
+    private func setOutlets() {
         backgroundColor = .red
         layer.cornerRadius = 10
         clipsToBounds = true
         layer.borderWidth = 1
         layer.borderColor = UIColor.black.cgColor
+        contentView.addSubview(imageViewCell)
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    private func setConstraints() {
+        imageViewCell.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
